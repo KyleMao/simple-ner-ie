e4136ee94d1e888a1cf6c755ba3d9330748bdd41
KyleMao <maozeximzx@gmail.com>
2014-11-23 13:24:59 -0500

    Add pre-trained NER model

--- ner_ie/ner.py
+++ ner_ie/ner.py
@@ -1,6 +1,7 @@
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '../lib'))
+PROJECT_ROOT = os.path.join(os.path.dirname(__file__), '..')
+sys.path.append(os.path.join(PROJECT_ROOT, 'lib'))
 from mitie import *
 
 def ner(sents):
@@ -10,6 +11,8 @@ def ner(sents):
         A list of lowercased sentence strings.
 
     """
+
+    extractor = named_entity_extractor(os.path.join(PROJECT_ROOT, 'data/models/ner_model.dat'))
 
     for sent in sents:
         tokens = tokenize(sent)
